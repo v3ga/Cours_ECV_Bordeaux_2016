b2a1824cc88ea4a632ee14f510e600beeb552137
v3ga <julien@v3ga.net>
2016-04-01 10:32:09 +0200

    updates

--- 09/02-svg/sketch.js
+++ 09/02-svg/sketch.js
@@ -10,7 +10,7 @@ function setup()
 {
 	createCanvas(800,800);
 
-	points = sampleText(font,"hey",5,10,500,500);
+	points = sampleText(font,"p5js",15,10,500,500);
 }
 
 function draw()
@@ -18,7 +18,7 @@ function draw()
 	background(255);
 	for (var p of points)
 	{
-		d = dist(mouseX,mouseY,p.x,p.y)/5;
+		d = 5;//dist(mouseX,mouseY,p.x,p.y)/5;
 		ellipse( p.x, p.y, d,d );
 	}
 //	noLoop();
